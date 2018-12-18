97a67f852cd17f46d69514a81783cdc947fd373b
Mikhail <martmiel@gmail.com>
2018-12-18 23:42:38 +0200

    use shortcut to render template

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-publication_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     resp = "You're looking at question %s." 
