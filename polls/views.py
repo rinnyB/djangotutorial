41ca2ec53c538338f3725d0998ca734f41cda42a
Mikhail <martmiel@gmail.com>
2018-12-18 23:37:56 +0200

    add template for index, modify index view to use template

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Question
 
 def index(request):
-    resp = "Hello World. You are at polls index!"
-    return HttpResponse(resp)
+    latest_question_list = Question.objects.order_by("-publication_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     resp = "You're looking at question %s." 
