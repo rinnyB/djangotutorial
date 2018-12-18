acdd0ceb0ceded885de826296b1bcaf9d1c44ef3
Mikhail <martmiel@gmail.com>
2018-12-18 21:59:07 +0200

    fix question_text name

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_test = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField("date published")
 
 
