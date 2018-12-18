958d740359cb3b7f835fecaa4dae11fb8648b0f5
Mikhail <martmiel@gmail.com>
2018-12-18 23:57:50 +0200

    add detail template, upd detail view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.http import Http404
+
+
 
 
 from .models import Question
@@ -11,8 +12,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    resp = "You're looking at question %s." 
-    return HttpResponse(resp % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     resp = "You're looking at results of question %s"
