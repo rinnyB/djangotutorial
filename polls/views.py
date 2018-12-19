6912968c92c2422956d9cd9ee508080d08ce4b28
Mikhail <martmiel@gmail.com>
2018-12-19 20:58:44 +0200

    add results.html template implement results view

--- polls/views.py
+++ polls/views.py
@@ -18,12 +18,11 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    resp = "You're looking at results of question %s"
-    return HttpResponse(resp % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    # resp = "You"re voting on question %s."
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -32,7 +31,7 @@ def vote(request, question_id):
                       "polls/detail.html",
                       {
                           "question": question,
-                          "error_message": "You didn't select a choice"
+                          "error_message": "You didn't select a choice!"
                       })
     else:
         selected_choice.votes += 1
@@ -40,5 +39,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results',
+        return HttpResponseRedirect(reverse("polls:results",
                                             args=(question.id,)))
